182fd6e450985dcdef3cbfc6f173a8a985e8cca9
giovannybrandalise <giovannybrandalise@brq.com>
2021-09-06 16:41:51 -0300

    corrigindo chamada da funcao promise

--- promise.test.js
+++ promise.test.js
@@ -1,7 +1,7 @@
 const promiseFunction = require('./promise');
 
 test('promise function returns correct value', () => {
-    return promiseFunction(data => {
+    return promiseFunction().then(data => {
         expect(data).toBe('completed');
     });
 });
